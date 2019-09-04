4004f77a17dbee30624d1a055c7ac05a18905fa0
Chi-Kang-Kuo <woichkoxi@gmail.com>
2019-09-04 20:41:31 +0800

    fix code for like readme

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -1,3 +1,3 @@
 # TODO: create shell script for running your YoloV1-vgg16bn model
-#python hw2_evaluation_task.py <PredictionDir> <AnnotationDir>
-python hw2_evaluation_task.py ./save/vgg16_bn/predict/ ./hw2_train_val/val1500/labelTxt_hbb
+# bash ./hw2.sh ./hw2_train_val/val1500/images ./save/vgg16_bn/predict/
+python3 predict.py -img=$1 -save=$2 -cfg=./models/vgg16_bn/config.py -model=./save/vgg16_bn/epoch_59_loss_547.597.pth 
---
+++ hw2/hw2_evaluation.sh
@@ -0,0 +1,2 @@
+#python hw2_evaluation_task.py <PredictionDir> <AnnotationDir>
+python hw2_evaluation_task.py ./save/vgg16_bn/predict/ ./hw2_train_val/val1500/labelTxt_hbb
